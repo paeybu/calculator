5c8ea1ad2486ca30541acf66640c45e11a14e8a0
Vachirarat Thavisaengsiri <pae9769@gmail.com>
2019-06-26 20:07:55 +0700

    init

---
+++ app/src/main/java/com/kabu/kabi/calculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kabu.kabi.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
